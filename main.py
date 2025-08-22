42a9aeebcd69af713d8999c458a9d326cb823198
shadeMB <shade_mbogdan@yahoo.com>
2025-08-22 14:45:42 +0300

    added word count functionality

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-def get_book_text(filepath):
-    with open(filepath) as f:
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
+
+def get_book_text(path):
+    with open(path) as f:
         return f.read()
 
-def main(filepath):
-    print(get_book_text(filepath))
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
-main('books/frankenstein.txt')
+main()
