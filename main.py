74e4fbaa48958898f2b42b0d0f351d1c88a26af5
shadeMB <shade_mbogdan@yahoo.com>
2025-08-23 17:20:18 +0300

    added argument support

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import (
     get_num_words,
     get_num_characters,
@@ -5,7 +6,11 @@ from stats import (
 )
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)             # get the book text
     num_words = get_num_words(text)             # find number of words in book text
     char_counts = get_num_characters(text)      # dict containing chars and the count for each one
